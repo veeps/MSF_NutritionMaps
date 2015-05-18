a0be0f6481c884c4e15945b88e1f03ca8e37bbef
Vivian Peng <vpeng12@gmail.com>
2015-05-18 09:43:20 -0400

    overview map in AI

--- mapping.R
+++ mapping.R
@@ -3,9 +3,7 @@ library(useful)
 require(stringr)
 library(rworldmap)
 library(RColorBrewer)
-library(ggplot2)
-library(ggmap)
-library(raster)
+
 ?RColorBrewer
 #read in data files
 nutrition <- read.table("2014admissions.csv", sep=",", header = TRUE, stringsAsFactors = FALSE, na.strings=c("NA", "NULL"))
@@ -22,13 +20,14 @@ head(mapdata)
 #map heatmap
 #overview map
 heatmap <- joinCountryData2Map (mapdata, joinCode = "ISO3", nameJoinColumn = "CountryCode")
-mapCountryData(mapToPlot = heatmap, mapRegion = "world", nameColumnToPlot = "Admissions", catMethod  = "pretty", addLegend = FALSE ,oceanCol = "white", lwd = 0.5, colourPalette = c("#FEE5D9", "#FCAE91", "#FB6A4A", "#DE2D26", "#A50F15"), numCats = 5 )
+mapCountryData(mapToPlot = heatmap, mapRegion = "world", nameColumnToPlot = "Admissions", catMethod  = "quantiles", addLegend = FALSE ,oceanCol = "white", lwd = 0.5, colourPalette = c("#efd3d3", "#f47171", "#e22634", "#bb1c29", "#940f1a", "#560404"), numCats = 6 )
 
 #get color palette from RColorBrewer
-brewer.pal(5, "Reds")
+# brewer.pal(5, "Reds")
+# decided to match colors from last year's graph instead of using colorbrewer
 
 #add country names to map
-labelCountries(heatmap, nameCountryColumn = "Country", cex = .3 , col= "black")
+labelCountries(heatmap, nameCountryColumn = "Country", cex = .1 , col= "black")
 
 #add legend boxes
 addMapLegendBoxes(cutVector = c("75,000+",
@@ -37,5 +36,5 @@ addMapLegendBoxes(cutVector = c("75,000+",
                                 "5,000-10,000",
                                 "1,000-5,000",
                                 "0-1000"
-                ),colourVector = c("#FEE5D9", "#FCAE91", "#FB6A4A", "#DE2D26", "#A50F15"),
-                 cex = 0.5, pt.cex = 1, title="Total Admissions")
+                ),colourVector = c("#efd3d3", "#f47171", "#e22634", "#bb1c29", "#940f1a", "#560404"),
+                 cex = 0.2, pt.cex = 1, title="Total Admissions")
